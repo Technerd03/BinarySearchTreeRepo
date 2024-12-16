5eafbab1e2f5ff436160bc9568fe0cd970db9d88
Technerd03 <151402054+Technerd03@users.noreply.github.com>
2024-12-15 20:35:22 -0330

    Delete binarysearchtreeapp/bst-app directory

--- binarysearchtreeapp/bst-app/src/main/java/com/alex/bst/repository/TreeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alex.bst.repository;
-
-import com.alex.bst.model.StoredTree;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TreeRepository extends JpaRepository<StoredTree, Long> {}
